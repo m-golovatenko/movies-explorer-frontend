d04f2358ddd61840c24fb7b5119fdf327b1b5921
Maria Golovatenko <pikch4y@yandex.com>
2023-08-28 13:39:54 +0300

    feat: add a border bottom to each block

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Portfolio.css';
 
 function Portfolio() {
-  return <div></div>;
+  return (
+    <div className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item"></li>
+      </ul>
+    </div>
+  );
 }
 
 export default Portfolio;
