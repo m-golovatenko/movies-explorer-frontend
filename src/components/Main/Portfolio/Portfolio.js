7e1a0e55261ead642587105ffb7f92616f3698ba
Maria Golovatenko <pikch4y@yandex.com>
2023-08-28 14:01:04 +0300

    feat: add portfolio layout

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,7 +6,39 @@ function Portfolio() {
     <div className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
-        <li className="portfolio__list-item"></li>
+        <li className="portfolio__list-item">
+          <a
+            href="https://github.com/m-golovatenko/how-to-learn"
+            target="_blank"
+            className="portfolio__list-item-link"
+            rel="noreferrer"
+          >
+            <h3 className="portfolio__list-item-link-title">Статичный сайт</h3>
+            <div className="portfolio__list-item-link-icon"></div>
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            href="https://m-golovatenko.github.io/second-project-travel/"
+            target="_blank"
+            className="portfolio__list-item-link"
+            rel="noreferrer"
+          >
+            <h3 className="portfolio__list-item-link-title">Адаптивный сайт</h3>
+            <div className="portfolio__list-item-link-icon"></div>
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            href="https://m-golovatenko.nomoreparties.co/"
+            target="_blank"
+            className="portfolio__list-item-link"
+            rel="noreferrer"
+          >
+            <h3 className="portfolio__list-item-link-title">Одностраничное приложение</h3>
+            <div className="portfolio__list-item-link-icon"></div>
+          </a>
+        </li>
       </ul>
     </div>
   );
