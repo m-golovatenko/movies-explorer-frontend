a04d12cd3638f999417e2242435de4eaf5593c91
Maria Golovatenko <pikch4y@yandex.com>
2023-08-28 11:35:22 +0300

    feat: add about project layout

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import './AboutProject.css';
 
 function AboutProject() {
-  return <div></div>;
+  return (
+    <div className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <ul className="about-project__list">
+        <li className="about-project__list-item">
+          <h3 className="about-project__list-item-title">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__list-item-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
+            доработки.
+          </p>
+        </li>
+        <li className="about-project__list-item">
+          <h3 className="about-project__list-item-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__list-item-text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
+            успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <ul className="about-project__schedule">
+        <li className="about-project__schedule-item">
+          <p className="about-project__schedule-item-title about-project__schedule-item-title_short">
+            1 неделя
+          </p>
+          <p className="about-project__schedule-item-subtitle">Back-end</p>
+        </li>
+        <li className="about-project__schedule-item">
+          <p className="about-project__schedule-item-title about-project__schedule-item-title_long">
+            4 недели
+          </p>
+          <p className="about-project__schedule-item-subtitle">Front-end</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default AboutProject;
--- src/components/Main/NavTab/NavTab.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NavTab() {
-  return <div></div>;
-}
-
-export default NavTab;
