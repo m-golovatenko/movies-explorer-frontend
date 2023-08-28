f1fc5758ae075d1789cd5e9bbec4116ee99623a5
Maria Golovatenko <pikch4y@yandex.com>
2023-08-28 14:14:16 +0300

    feat: add footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__content">
+        <p className="footer__content-copyright">&copy; 2023</p>
+        <ul className="footer__content-links">
+          <li>
+            <a
+              href="https://practicum.yandex.ru/"
+              target="_blank"
+              className="footer__content-links-item"
+              rel="noreferrer"
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://github.com/m-golovatenko"
+              target="_blank"
+              className="footer__content-links-item"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
