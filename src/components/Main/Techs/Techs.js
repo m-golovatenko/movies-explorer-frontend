7b21749255b817219d254a6c6325ed77bfeca644
Maria Golovatenko <pikch4y@yandex.com>
2023-08-29 14:40:34 +0300

    feat: add tablet layout for techs section

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -5,22 +5,22 @@ import SectionHeading from '../SectionHeading/SectionHeading';
 function Techs() {
   return (
     <section className="techs">
-      <div className="techs-container">
+      <div className="techs__container">
         <SectionHeading titleText="Технологии" />
-        <div className="techs-container-content">
-          <h3 className="techs-container-content-title">7 технологий</h3>
-          <p className="techs-container-content-text">
+        <div className="techs__container-content">
+          <h3 className="techs__container-content-title">7 технологий</h3>
+          <p className="techs__container-content-text">
             На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
           </p>
         </div>
-        <ul className="techs-container-list">
-          <li className="techs-container-list-item">HTML</li>
-          <li className="techs-container-list-item">CSS</li>
-          <li className="techs-container-list-item">JS</li>
-          <li className="techs-container-list-item">REACT</li>
-          <li className="techs-container-list-item">Git</li>
-          <li className="techs-container-list-item">Express.js</li>
-          <li className="techs-container-list-item">mongoDB</li>
+        <ul className="techs__container-list">
+          <li className="techs__container-list-item techs__container-list-item_upper">Html</li>
+          <li className="techs__container-list-item techs__container-list-item_upper">Css</li>
+          <li className="techs__container-list-item techs__container-list-item_upper">Js</li>
+          <li className="techs__container-list-item">React</li>
+          <li className="techs__container-list-item">Git</li>
+          <li className="techs__container-list-item">Express.js</li>
+          <li className="techs__container-list-item">mongoDB</li>
         </ul>
       </div>
     </section>
